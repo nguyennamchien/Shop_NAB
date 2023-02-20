789e35fe73ccc1658624dded214bac446de04e69
nguyennamchien <namchien05022001@gmail.com>
2023-02-20 11:03:21 +0700

    Upload Project first time

---
+++ Shop_NAB/Shop_NAB/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop_NAB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hello bạn đây là shop của tuiiii.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hãy gọi cho tôi đi mà ";
+            ViewBag.Message = "Phone : 087425401";
+
+
+
+            return View();
+        }
+    }
+
+}
---
+++ Shop_NAB/Shop_NAB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shop_NAB.MvcApplication" Language="C#" %>
